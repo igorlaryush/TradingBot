d1dde3a6f477245a305bbc3f6359a980366c99b2
igorlaryush <49840601+igorlaryush@users.noreply.github.com>
2021-04-28 14:00:11 +0300

    added Bitmex support

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import logging
 
 from connectors.binance_futures import BinanceFuturesClient
-
+from connectors.bitmex import BitmexClient
 
 logger = logging.getLogger()
 
@@ -20,13 +20,11 @@ file_handler.setLevel(logging.DEBUG)
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 
-
 if __name__ == '__main__':
-
-    binance = BinanceFuturesClient('public_key',
-                                   'secret_key',
-                                   True)
-    print(binance.get_historical_candles('BTCUSDT', '1h'))
+    binance = BinanceFuturesClient('ab83af351da6fe25c83d5fa06d4ced6772f2ec3c3b74950c64ce98ab7d94c2f3',
+                                   '059317a743f4da70e55ad5b30db1a110a276886c76d67e40b28fb10fdaa4ef97', True)
+    #bitmex = BitmexClient("uXr1T711wD-", "", True)
+    print(binance.get_balances()['BNB'].wallet_balance)
 
     root = tk.Tk()
     root.mainloop()
